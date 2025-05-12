e86d1deae2ecfebcf53cadfc9caaaa37874c52aa
sizem-re <taylorsizem.re@gmail.com>
2025-05-12 16:57:05 -0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,3 @@
 export default function NotFound() {
-  return (
-    <div style={{ textAlign: "center", padding: "4rem" }}>
-      <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>404 – Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/" style={{ color: "#0070f3", textDecoration: "underline", marginTop: "2rem", display: "inline-block" }}>
-        Go Home
-      </a>
-    </div>
-  );
+  return "404 – Page Not Found";
 } 
