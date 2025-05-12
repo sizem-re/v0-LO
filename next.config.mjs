f2314eef56e6f2fbdb94dd709465cebaae79c316
sizem-re <taylorsizem.re@gmail.com>
2025-05-12 16:30:50 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['i.imgur.com', 'cdn.warpcast.com', 'res.cloudinary.com'],
     unoptimized: true,
   },
-  // Specify the Node.js version for Vercel
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+  serverExternalPackages: [],
   eslint: {
     ignoreDuringBuilds: true,
   },
